d75fa6882b613bf961e7b6621eb8891fcc429d09
warmsmile <18773217005@163.com>
2018-08-02 14:57:30 +0800

    009

--- WeChat/js/productDetails/account.js
+++ WeChat/js/productDetails/account.js
@@ -7,3 +7,33 @@ function setAccountSize()
 {
 	$id("act").style.height=aspectRatio.snHeight()+"px";
 }
+
+
+var choice=$className("choice");
+
+var wayb=$className("act-way-bar");
+
+var index=0;
+for(var i=0;i<wayb.length;i++)
+{
+	
+	(function(i){
+		wayb[i].onclick=function(){
+			choice[index+1].style.display="none";
+			choice[i+1].style.display="inline-block";
+			index=i;
+		}
+	})(i)
+}
+
+var flag=true;
+$id("default").onclick=function()
+{
+	if(flag){
+		choice[0].src="../../img/drawable-hdpi/account/yes.png";
+		flag=false;
+	}else{
+		choice[0].src="../../img/drawable-hdpi/account/no.png";
+		flag=true;
+	}
+}
