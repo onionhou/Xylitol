149a2015d2bc6ee6ec45a5aadaff34c4ffff3b45
warmsmile <18773217005@163.com>
2018-08-06 15:35:03 +0800

    009

--- WeChat/js/homepage/index.js
+++ WeChat/js/homepage/index.js
@@ -249,6 +249,22 @@ function getLocation()
 	{
 		var latitde=position.coords.latitude;
 		var longitude=position.coords.longitude;
-		alert(latitde+"****"+longitude);
+		var ggPoint=new BMap.Point(latitde,longitude);
+		var bmap = new BMap.GeoPosition();
+		bmap.getCurrentPosition(function(r)
+		{
+			if(this.getStatus() == BMAP_STATUS_SUCCESS)
+			{
+				var mk = new BMap.Marker(r.point);
+				map.addOverlay(mk);
+				map.panTo(r.point);
+				alert('您的位置：'+r.point.lng+','+r.point.lat);
+			}
+			else 
+			{
+				alert('failed'+this.getStatus());
+			}        
+		});
+		
 	}
 }
